80ca0d9f472d6db469d24b99398f2c88b10b2151
lunispang <linus.wennborn@outlook.com>
2024-07-25 11:57:10 +0200

    add telescope stuff and rustaceanvim

--- lua/plugins/spec1.lua
+++ lua/plugins/spec1.lua
@@ -7,11 +7,14 @@ return {
 	{'hrsh7th/nvim-cmp'},
 	{'hrsh7th/cmp-nvim-lsp'},
 	{'hrsh7th/cmp-buffer'},
+	{'hrsh7th/cmp-path'},
 	{'hrsh7th/cmp-vsnip'},
 	{'hrsh7th/vim-vsnip'},
 
 	{'folke/tokyonight.nvim'},
 	{'folke/which-key.nvim'},
 
-	{'nvim-telescope/telescope.nvim'}
+	{'nvim-telescope/telescope.nvim'},
+
+	{'mrcjkb/rustaceanvim'}
 }
